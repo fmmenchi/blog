1cdf7e6fd0feb4ee76f4744f68f609c8f22440d1
Fabio Menchicchi <f.menchicchi@iungo.it>
2023-12-30 02:22:41 -0300

    feat(components): add tailwind support

--- libs/components/src/lib/components.tsx
+++ libs/components/src/lib/components.tsx
@@ -1,13 +1,12 @@
-import styles from './components.module.css';
 
 /* eslint-disable-next-line */
 export interface ComponentsProps {}
 
 export function Components(props: ComponentsProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Components!</h1>
-    </div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
   );
 }
 
